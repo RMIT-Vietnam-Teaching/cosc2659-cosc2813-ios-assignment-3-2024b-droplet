ed7352a0eef67d2792eed83970acd354296a57b9
Pham Hoang Long <s3938007@rmit.edu.vn>
2024-09-04 15:29:35 +0700

    setup project folder

---
+++ pharmacist_project/pharmacist_project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pharmacist_project
+//
+//  Created by Long Pham Hoang on 4/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pharmacist_project/pharmacist_project/Views/Authentication/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  pharmacist_project
+//
+//  Created by Long Pham Hoang on 4/9/24.
+//
+
+import Foundation
