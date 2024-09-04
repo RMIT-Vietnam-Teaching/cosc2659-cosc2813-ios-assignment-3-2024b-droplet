553bc9e35544bfc04d8082f5b6784cac7a980fdb
Leon Do <91256597+Random-Palico@users.noreply.github.com>
2024-09-04 19:47:35 +0700

    format

--- pharmacist_project/pharmacist_project/ContentView.swift
+++ pharmacist_project/pharmacist_project/ContentView.swift
@@ -1,15 +1,15 @@
 /*
-  RMIT University Vietnam
-  Course: COSC2659 iOS Development
-  Semester: 2023B
-  Assessment: Assignment 3
-  Author:
-    Long Pham Hoang
-    Do Phan Nhat Anh (s3915034)
-  Created  date: 04/09/2024
-  Last modified: 04/09/2024
-  Acknowledgement:
-*/
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2023B
+ Assessment: Assignment 3
+ Author:
+ Long Pham Hoang
+ Do Phan Nhat Anh (s3915034)
+ Created  date: 04/09/2024
+ Last modified: 04/09/2024
+ Acknowledgement:
+ */
 
 import SwiftUI
 
