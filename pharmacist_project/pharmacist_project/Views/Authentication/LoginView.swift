1f03634f4604d57b67a53e4b52476c875f8c8cab
Leon Do <91256597+Random-Palico@users.noreply.github.com>
2024-09-05 14:30:41 +0700

    update the register and login

--- pharmacist_project/pharmacist_project/Views/Authentication/LoginView.swift
+++ pharmacist_project/pharmacist_project/Views/Authentication/LoginView.swift
@@ -19,93 +19,119 @@ struct LoginScreenView: View {
     @State private var email = ""
     @State private var password = ""
     
+    @State private var navigateToRegister = false
+    
     var body: some View {
-        NavigationStack {
-            VStack {
-                Text("Login")
-                    .font(.largeTitle)
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .padding(.top, 50)
-                
-                Spacer()
-                
-                VStack(spacing: 16) {
-                    TextField("Email", text: $email)
-                        .padding()
-                        .background(Color(UIColor.systemGray6))
-                        .cornerRadius(8)
-                        .padding(.horizontal)
+        GeometryReader { geometry in
+            NavigationStack {
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.top, geometry.size.height * 0.05)
                     
-                    SecureField("Password", text: $password)
-                        .padding()
-                        .background(Color(UIColor.systemGray6))
-                        .cornerRadius(8)
-                        .padding(.horizontal)
-                }
-                
-                HStack {
                     Spacer()
-                    Button(action: {
-                        // Forgot Password
-                    }) {
-                        Text("Forgot your password?")
-                            .font(.body)
-                            .foregroundColor(.blue)
-                    }
-                    .padding(.trailing)
-                }
-                .padding(.top, 8)
-                
-                // Login Button
-                Button(action: {
-                    // Login action
-                }) {
-                    Text("LOGIN")
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color(hex: "2EB5FA"))
-                        .foregroundColor(.white)
-                        .cornerRadius(20)
-                        .shadow(radius: 4)
-                }
-                .padding(.horizontal)
-                .padding(.top, 24)
-                
-                Text("Or login with social account")
-                    .font(.body)
-                    .foregroundColor(.gray)
-                    .padding(.top, 24)
-                
-                // Login with social account buttons
-                HStack(spacing: 16) {
-                    Button(action: {
-                        // Google login
-                    }) {
-                        Image("GoogleIcon")
-                            .resizable()
-                            .padding(10)
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
-                            .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                    
+                    VStack(spacing: 16) {
+                        TextField("Email", text: $email)
+                            .padding()
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(8)
+                            .padding(.horizontal)
+                        
+                        SecureField("Password", text: $password)
+                            .padding()
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(8)
+                            .padding(.horizontal)
                     }
                     
-                    Button(action: {
-                        // Facebook login
-                    }) {
-                        Image("FacebookIcon")
-                            .resizable()
-                            .padding(10)
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
-                            .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            // Forgot Password
+                        }) {
+                            Text("Forgot your password?")
+                                .font(.body)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(.trailing)
                     }
+                    .padding(.top, 8)
+                    
+                    // Login Button
+                    Button(action: {
+                        // Login action
+                    }) {
+                        Text("Login")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color(hex: "2EB5FA"))
+                            .foregroundColor(.white)
+                            .cornerRadius(20)
+                            .shadow(radius: 4)
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 10)
+                    
+                    // Sign Up Button
+                    Button(action: {
+                        navigateToRegister = true
+                    }) {
+                        Text("Sign up")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .foregroundColor(Color(hex: "2EB5FA"))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 20)
+                                    .stroke(Color(hex: "2EB5FA"), lineWidth: 2)
+                            )
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 5)
+                    
+                    Spacer()
+                    
+                    // login with social
+                    Text("Or login with social account")
+                        .font(.body)
+                        .foregroundColor(.gray)
+                        .padding(.bottom, 8)
+                    
+                    // Login with social account buttons
+                    HStack(spacing: 16) {
+                        Button(action: {
+                            // Google login
+                        }) {
+                            Image("GoogleIcon")
+                                .interpolation(.none)
+                                .resizable()
+                                .padding(15)
+                                .frame(width: geometry.size.width * 0.12, height: geometry.size.width * 0.12)
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                        }
+                        
+                        Button(action: {
+                            // Facebook login
+                        }) {
+                            Image("FacebookIcon")
+                                .resizable()
+                                .padding(15)
+                                .frame(width: geometry.size.width * 0.12, height: geometry.size.width * 0.12)
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+                        }
+                    }
+                    .padding(.bottom, geometry.size.height * 0.02)
                 }
-                .padding(.top, 16)
-                
-                Spacer()
+                .background(Color(hex: "F9F9F9"))
+                .navigationBarBackButtonHidden(true)
+                .navigationDestination(isPresented: $navigateToRegister) {
+                    RegisterScreenView()
+                }
             }
-            .background(Color(hex: "F9F9F9"))
         }
     }
 }
