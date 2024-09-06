ece58794289310286fda7aa7705c834e20f7975d
Pham Hoang Long <s3938007@rmit.edu.vn>
2024-09-06 16:25:05 +0700

    fix the CartItemCardView bug

--- pharmacist_project/pharmacist_project/Models/CartItem.swift
+++ pharmacist_project/pharmacist_project/Models/CartItem.swift
@@ -9,8 +9,23 @@ import Foundation
 
 struct CartItem: Identifiable {
     var id: String
+    var userId: String
     var name: String
     var image: String
     var quantity: Int
     var price: Double
+    
+    mutating func increaseQuantity() {
+        self.quantity += 1
+    }
+
+    mutating func decreaseQuantity() -> Bool {
+        if self.quantity > 1 {
+            self.quantity -= 1
+            return false
+        } else {
+            self.quantity = 0
+            return true
+        }
+    }
 }
--- pharmacist_project/pharmacist_project/Views/Component/CartItemCardView.swift
+++ pharmacist_project/pharmacist_project/Views/Component/CartItemCardView.swift
@@ -14,20 +14,29 @@
 import SwiftUI
 
 struct CartItemCardView: View {
-    let cartItem: CartItem
+    @State var cartItem: CartItem // can modify this later to binding
     
     var body: some View {
         ZStack(alignment: .topTrailing) {
             HStack(spacing: 16) {
                 VStack {
-                    Image(cartItem.image)
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(8)
+                    AsyncImage(url: URL(string: cartItem.image)) { phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .interpolation(.none)
+                                .frame(width: 100, height: 100)
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(8)
+                        } else if phase.error != nil {
+                            Color.gray.frame(width: 100, height: 100)
+                        } else {
+                            ProgressView().frame(width: 100, height: 100)
+                        }
+                    }
                     
                     HStack(spacing: 10) {
-                        Button(action: {}) {
+                        Button(action: {cartItem.decreaseQuantity()}) {
                             Image(systemName: "minus.square")
                                 .font(.title2)
                                 .frame(width: 18, height: 18)
@@ -38,7 +47,7 @@ struct CartItemCardView: View {
                             .font(.headline)
                             .frame(width: 40, height: 20)
                         
-                        Button(action: {}) {
+                        Button(action: {cartItem.increaseQuantity()}) {
                             Image(systemName: "plus.square")
                                 .font(.title2)
                                 .frame(width: 18, height: 18)
@@ -59,7 +68,7 @@ struct CartItemCardView: View {
 
                     HStack {
                         Spacer()
-                        Text("\(Int(cartItem.price)) ₫")
+                        Text("\((cartItem.price*Double(cartItem.quantity)).formatAsCurrency())")
                             .font(.system(size: 16))
                             .fontWeight(.bold)
                             .padding(.top, 4)
@@ -71,17 +80,18 @@ struct CartItemCardView: View {
             .padding()
             .background(Color.white)
             .cornerRadius(10)
-            .frame(width: 328, height: 167)
+            .frame(width: 380, height: 150)
             .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
         }
     }
 }
 
 #Preview {
-    let exampleCartItem = CartItem(
+    var exampleCartItem = CartItem(
         id: "1",
+        userId: "123",
         name: "Blackmores Omega Double High Strength Fish Oil bổ sung dầu cá",
-        image: "FishOil",
+        image: "https://prod-cdn.pharmacity.io/e-com/images/product/1000x1000/20240717073116-0-P24634_3.png",
         quantity: 1,
         price: 551650
     )
