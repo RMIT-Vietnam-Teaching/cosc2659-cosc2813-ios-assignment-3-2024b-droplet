3884c40c8384b7fa5b3044e38fe085a96baba3ca
“thinh.ngo” <thinh.ngongoc@shopee.com>
2024-09-16 21:57:28 +0700

    add author header

--- pharmacist_project/pharmacist_project/Views/Component/OrderInfoView.swift
+++ pharmacist_project/pharmacist_project/Views/Component/OrderInfoView.swift
@@ -1,9 +1,27 @@
-//
-//  OrderInfoView.swift
-//  pharmacy
-//
-//  Created by Dinh Le Hong Tin on 7/9/24.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2024B
+  Assessment: Assignment 3
+  Author: Dinh Le Hong Tin
+  ID: s3932134
+  Created  date: 05/09/2024
+  Last modified: 16/09/2024
+  Acknowledgement:
+     https://rmit.instructure.com/courses/138616/modules/items/6274581
+     https://rmit.instructure.com/courses/138616/modules/items/6274582
+     https://rmit.instructure.com/courses/138616/modules/items/6274583
+     https://rmit.instructure.com/courses/138616/modules/items/6274584
+     https://rmit.instructure.com/courses/138616/modules/items/6274585
+     https://rmit.instructure.com/courses/138616/modules/items/6274586
+     https://rmit.instructure.com/courses/138616/modules/items/6274588
+     https://rmit.instructure.com/courses/138616/modules/items/6274589
+     https://rmit.instructure.com/courses/138616/modules/items/6274590
+     https://rmit.instructure.com/courses/138616/modules/items/6274591
+     https://rmit.instructure.com/courses/138616/modules/items/6274592
+     https://developer.apple.com/documentation/swift/
+     https://developer.apple.com/documentation/swiftui/
+*/
 
 import SwiftUI
 
